6b0a03f4cd8a2c1f62d456e915011d524b1e0a47
Alexander Bokovoy <abokovoy@redhat.com>
2025-12-10 10:42:02 +0200

    Fix strchr() conformance to C23
    C23 7.28.5.1 specifies search functions such as strchr() as generic,
    returning const char * if the first argument is of type const char *.
    Fix uses of strchr() to conform to this change.
    
    [jrische@redhat.com: altered changes to avoid casts; fixed an
    additional case]
    [ghudson@mit.edu: condensed some declarations; rewrote commit message]
    
    ticket: 9191 (new)
    (cherry picked from commit ad4dcf1856dadc4b352b5c8ff08e51c7290fb41f)

--- src/plugins/preauth/pkinit/pkinit_crypto_openssl.c
+++ src/plugins/preauth/pkinit/pkinit_crypto_openssl.c
@@ -5006,7 +5006,7 @@ load_cas_and_crls(krb5_context context,
                   pkinit_req_crypto_context req_cryptoctx,
                   pkinit_identity_crypto_context id_cryptoctx,
                   int catype,
-                  char *filename)
+                  const char *filename)
 {
     STACK_OF(X509_INFO) *sk = NULL;
     STACK_OF(X509) *ca_certs = NULL;
@@ -5164,7 +5164,7 @@ load_cas_and_crls_dir(krb5_context context,
                       pkinit_req_crypto_context req_cryptoctx,
                       pkinit_identity_crypto_context id_cryptoctx,
                       int catype,
-                      char *dirname)
+                      const char *dirname)
 {
     krb5_error_code retval = EINVAL;
     char **fnames = NULL, *filename;
@@ -5208,7 +5208,7 @@ crypto_load_cas_and_crls(krb5_context context,
                          pkinit_identity_crypto_context id_cryptoctx,
                          int idtype,
                          int catype,
-                         char *id)
+                         const char *id)
 {
     switch (idtype) {
     case IDTYPE_FILE:
